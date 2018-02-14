af10694ea7e01ff771b869de3534877c3bbbcd8e
Vivek Nain <vivek@p3infotech.in>
2018-02-14 11:02:54 +0530

    added django.shortcuts module

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,16 +1,16 @@
-from django.template.loader import get_template
-from django.template import Context
-from django.http import HttpResponse    
+from django.shortcuts import render_to_response  
 import datetime
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    t = get_template('current_datetime.html')
-    html = t.render(Context({'current_date': now}))
-    return HttpResponse(html)
+    return render_to_response('current_datetime.html', {'current_date': now})
+    #t = get_template('current_datetime.html')
+    #html = t.render(Context({'current_date': now}))
+    #return HttpResponse(html)
 
 def hours_ahead(request, offset):
     offset = int(offset)
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s</body></html>" % (offset,dt)
-    return HttpResponse(html)
+    return render_to_response('hours_ahead.html', {'current_date': dt, 'offset': offset})
+    #html = "<html><body>In %s hour(s), it will be %s</body></html>" % (offset,dt)
+    #return HttpResponse(html)
